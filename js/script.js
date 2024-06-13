9ea367e3c236d0278e14807ce17bf31d0a6dedff
Pedro Carvalho <pedrocarvalho3232@hotmail.com>
2024-06-13 13:10:01 -0300

    mapa

--- js/script.js
+++ js/script.js
@@ -8,11 +8,13 @@ document.getElementById('calcular').addEventListener('click', function() {
     const carneBovinaGramas = (homens * 500) + (mulheres * 300) + (criancas * 200);
     const frangoGramas = (homens * 200) + (mulheres * 200) + (criancas * 100);
     const linguicaGramas = (homens * 200) + (mulheres * 200) + (criancas * 200);
-    const refrigeranteMililitros = (homens * 300) + (mulheres * 400) + (criancas * 200);
-    const cervejaMililitros = (homens * 800) + (mulheres * 500);
 
-    // Conversão de gramas para quilogramas
-    const carneBovinaKg = (carneBovinaGramas / 1000).toFixed(2); 
+    // Cálculo das quantidades de refrigerante e cerveja em litros
+    const refrigeranteLitros = ((homens * 300) + (mulheres * 400) + (criancas * 200)) / 1000;
+    const cervejaLitros = ((homens * 800) + (mulheres * 500)) / 1000;
+
+    // Conversão de gramas para quilogramas para carne bovina, frango e linguiça
+    const carneBovinaKg = (carneBovinaGramas / 1000).toFixed(2); // Arredonda para 2 casas decimais
     const frangoKg = (frangoGramas / 1000).toFixed(2);
     const linguicaKg = (linguicaGramas / 1000).toFixed(2);
 
@@ -21,8 +23,8 @@ document.getElementById('calcular').addEventListener('click', function() {
         <p>Carne Bovina: ${carneBovinaKg} kg (${carneBovinaGramas} g)</p>
         <p>Frango: ${frangoKg} kg (${frangoGramas} g)</p>
         <p>Linguiça: ${linguicaKg} kg (${linguicaGramas} g)</p>
-        <p>Refrigerante: ${refrigeranteMililitros} ml</p>
-        <p>Cerveja: ${cervejaMililitros} ml</p>
+        <p>Refrigerante: ${refrigeranteLitros.toFixed(2)} litros</p>
+        <p>Cerveja: ${cervejaLitros.toFixed(2)} litros</p>
     `;
 
     // Atualização do conteúdo do elemento #result
